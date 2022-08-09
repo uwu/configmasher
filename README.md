51adf90ba214d3cfb1d58d10dac634113c723b40
Phorcys <57866459+phorcys420@users.noreply.github.com>
2022-08-09 12:38:18 +0200

    Fix typedefs & bump to 2.1.1 (very chaotic versioning)

--- README.md
+++ README.md
@@ -59,7 +59,7 @@ console.log(config);
 
 - [ ] Proper documentation (can't be bothered atm, just use typedefs)
 - [X] Allow as many underscores as the user wants between name and value : ``${NAME}_${VAL}``, ``${NAME}__${VAL}``, ``${NAME}_________${VAL}``
-- [ ] Automatically try to get and parse ${NAME}.json and toLowerCase if it exists
+- [X] Automatically try to get and parse ${NAME}.json and toLowerCase if it exists
 - [X] Allow the use of a proxy to make keys case-insensitive
 - [ ] Support adding custom config formats
 - [ ] Support CJS and ESM config
--- rollup.config.js
+++ rollup.config.js
@@ -2,32 +2,42 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 
-import tsPlugin from "rollup-plugin-typescript2";
+import tsPlugin from "@rollup/plugin-typescript";
 import esbuild from "rollup-plugin-esbuild";
 
-export default {
-  input: "src/index.ts",
-  output: [
-		{
-			file: "dist/index.mjs",
-			format: "es"
-		},
-		{
-			file: "dist/index.cjs",
-			format: "cjs",
-			exports: "default"
-		}
-	],
+import dts from "rollup-plugin-dts";
 
-	plugins: [
-		tsPlugin(),
-		
-		json(),
-		commonjs(),
-		nodeResolve(),
-		
-		esbuild({
-			minify: true
-		})
-	]
-};
+export default [
+	{
+		input: "src/index.ts",
+		output: [
+			{
+				file: "dist/index.mjs",
+				format: "es"
+			},
+			{
+				file: "dist/index.cjs",
+				format: "cjs",
+				exports: "default"
+			}
+		],
+	
+		plugins: [
+			tsPlugin(),
+			
+			json(),
+			commonjs(),
+			nodeResolve(),
+			
+			esbuild({
+				minify: true
+			})
+		]
+	},
+
+	{
+    input: "./dist/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  }
+]
